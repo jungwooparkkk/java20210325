2e6015bef927cd669cd3abbb42924d3da82bfb46
jungwooparkkk <pjw7935@naver.com>
2021-04-02 12:09:53 +0900

    Array

--- src/p05/lecture/A12RefTypeArray.java
+++ src/p05/lecture/A12RefTypeArray.java
@@ -1,5 +1,17 @@
 package p05.lecture;
 
 public class A12RefTypeArray {
-
+	// 그림 : 07.참조타입배열.pug
+	public static void main(String[] args) {
+		int[] arr1 = new int[3];
+		
+		String[] arr2 = new String[2];
+		
+		
+		arr1[0] = 3;
+		
+		arr2[0] = "java";
+		arr2[1] = new String("java");
+		
+	}
 }
