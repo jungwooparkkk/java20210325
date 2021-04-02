e7334de18222c97b71a322c69316a2f6a4b537bf
jungwooparkkk <pjw7935@naver.com>
2021-04-02 10:19:48 +0900

    ReferenceType

---
+++ src/p05/lecture/A10Array.java
@@ -0,0 +1,16 @@
+package p05.lecture;
+
+public class A10Array {
+	//그림 : 06.배열.png
+	public static void main(String[] args) {
+		//array(배열) : 여러개의 값을 갖는 자료형
+		
+		int i;
+		i = 5;
+		
+		int[] arr; // 배열도 참조타입
+		
+		
+		
+	}
+}
