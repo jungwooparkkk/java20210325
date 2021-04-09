a2e37a724e3fa2da8f2a9a021b64047e736cb638
Jungwooparkkk <pjw7935@naver.com>
2021-04-09 18:50:55 +0900

    method

---
+++ src/p06/lecture/p9encapsulation/MyClass.java
@@ -0,0 +1,39 @@
+package p06.lecture.p9encapsulation;
+
+public class MyClass {
+	private String str;
+	private int memoty;
+	private boolean on;
+	private String model;
+	private int ssd;
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public int getMemoty() {
+		return memoty;
+	}
+	public void setMemoty(int memoty) {
+		this.memoty = memoty;
+	}
+	public boolean isOn() {
+		return on;
+	}
+	public void setOn(boolean on) {
+		this.on = on;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getSsd() {
+		return ssd;
+	}
+	public void setSsd(int ssd) {
+		this.ssd = ssd;
+	}
+}
