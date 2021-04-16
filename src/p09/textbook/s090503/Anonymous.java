2cd21665b85cbc3d08c47efcfbed40e5efe61583
jungwooparkkk <pjw7935@naver.com>
2021-04-16 15:42:25 +0900

    Anonymous

---
+++ src/p09/textbook/s090503/Anonymous.java
@@ -0,0 +1,29 @@
+package p09.textbook.s090503;
+
+public class Anonymous {
+	private int field;
+	
+	public void method(final int arg1, int arg2) {
+		final int var1 = 0;
+		int var2 = 0;
+		
+		field = 10;
+		
+//		arg1 = 20; (X)
+//		arg2 = 20; (X)
+		
+		
+//		var1 = 30; (X)
+//		var2 = 30; (X)
+		
+		Calculatable calc = new Calculatable() {
+			@Override
+			public int sum() {
+				int result = field + arg1 + arg2 + var1 + var2;
+				return result;
+			}
+		};
+		System.out.println(calc.sum());
+		
+	}
+}
