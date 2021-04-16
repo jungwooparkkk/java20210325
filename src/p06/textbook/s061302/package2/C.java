84d46adf26dd12529188794590d85fc8c17b5c91
jungwooparkkk <pjw7935@naver.com>
2021-04-16 09:10:17 +0900

    encapsulation

--- src/p06/textbook/s061302/package2/C.java
+++ src/p06/textbook/s061302/package2/C.java
@@ -4,13 +4,9 @@ import p06.textbook.s061302.package1.A;
 
 public class C {
 	public C() {
-		A a = new A();
-		a.field1 = 1;
-		a.field2 = 1;
-		a.field3 = 1;
-		
-		a.method1();
-		a.method2();
-		a.method3();
+		A a1 = new A(true);
+		A a2 = new A(1);
+		A a3 = new A("문자열");
+		A a4;
 	}
-}
+
