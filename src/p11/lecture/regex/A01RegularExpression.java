95ccec2020c9a82aa0d85333389385f4927ee4a6
Jungwooparkkk <pjw7935@naver.com>
2021-04-22 14:28:06 +0900

    Pattern

---
+++ src/p11/lecture/regex/A01RegularExpression.java
@@ -0,0 +1,74 @@
+package p11.lecture.regex;
+
+import java.util.regex.Pattern;
+
+public class A01RegularExpression {
+	// 정규표현식
+	// 문자열의 패턴을 표현하는 문자열
+	// meta-character, quantifier
+
+	public static void main(String[] args) {
+		String regex1 = "a";
+		String str1 = "a";
+
+		boolean p1 = Pattern.matches(regex1, str1);
+		System.out.println(p1);
+
+		regex1 = "java";
+		str1 = "java";
+
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "9";
+		str1 = "0";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "\\d";
+		str1 = "8";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "\\d";
+		str1 = "89";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "\\d\\d";
+		str1 = "67";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "\\d{3}";
+		str1 = "999";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "\\d{3,}";
+		str1 = "982896";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "010-\\d{4}-\\d{4}";
+		str1 = "010-9876-3333";
+		System.out.println(Pattern.matches(regex1, str1));
+
+		regex1 = "\\d{3,5}"; //3~5개 사용가능
+		str1 = "22345";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d+"; // \d{1,}
+		str1 = "1";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d*"; // \d{0,}
+		str1 = "";
+		System.out.println(Pattern.matches(regex1, str1));
+	
+		regex1 = "\\d?"; // \d{0, 1}
+		str1 = "1";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "010-?\\d{4}-?\\d{4}";
+		str1 = "010-9876-3333";
+		String str2 = "01098763333";
+		String str3 = "010-98763333";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+	}
+}
---
+++ src/p11/textbook/exrecise/ex10/PatternMatcherExample.java
@@ -0,0 +1,18 @@
+package p11.textbook.exrecise.ex10;
+
+import java.util.regex.Pattern;
+
+public class PatternMatcherExample {
+	public static void main(String[] args) {
+		String id = "5Angel1004";
+		
+		//알파벳시작, 숫자알파벳, 총8~12
+		String regExp = "[a-zA-Z][a-zA-Z0-9]{7,11]";
+		boolean isMatch = Pattern.matches(regExp, id);
+		if(isMatch) {
+			System.out.println("아이디로 사용할 수 있습니다.");
+		} else {
+			System.out.println("아이디로 사용할 수 없습니다.");
+		}
+	}
+}
