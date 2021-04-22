6cf75044ebdd1ce02c639877efd438ba6045f600
Jungwooparkkk <pjw7935@naver.com>
2021-04-22 19:37:03 +0900

    Pattern

--- src/p11/textbook/exrecise/ex10/PatternMatcherExample.java
+++ src/p11/textbook/exrecise/ex10/PatternMatcherExample.java
@@ -7,7 +7,7 @@ public class PatternMatcherExample {
 		String id = "5Angel1004";
 		
 		//알파벳시작, 숫자알파벳, 총8~12
-		String regExp = "[a-zA-Z][a-zA-Z0-9]{7,11]";
+		String regExp = "[a-zA-Z][a-zA-Z0-9]{7,11}";
 		boolean isMatch = Pattern.matches(regExp, id);
 		if(isMatch) {
 			System.out.println("아이디로 사용할 수 있습니다.");
--- src/p11/textbook/s111002/PatternExample.java
+++ src/p11/textbook/s111002/PatternExample.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class PatternExample {
 	public static void main(String[] args) {
-		String regExp = "(02|010)-\\d{3, 4}-\\d{4}";
+		String regExp = "(02|010)-\\d{3,4}-\\d{4}";
 		String data = "010-123-4567";
 		boolean result = Pattern.matches(regExp, data);
 		if (result) {
