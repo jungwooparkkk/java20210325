bae10238045160b5c1b4f16527117acd566c50f2
Jungwooparkkk <pjw7935@naver.com>
2021-04-23 12:10:12 +0900

    Thread

---
+++ src/p12/textbook/s120201/BeepPrintExample3.java
@@ -0,0 +1,35 @@
+package p12.textbook.s120201;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample3 {
+	public static void main(String[] args) {
+		Runnable beepTask = new BeepTask();
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for (int i = 0; i < 5; i++) {
+					toolkit.beep();
+
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread.start();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("띵");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/p12/textbook/s120301/CalcThread.java
@@ -0,0 +1,7 @@
+package p12.textbook.s120301;
+
+public class CalcThread extends Thread {
+	public CalcThread() {
+		// TODO Auto-generated constructor stub
+	}
+}
